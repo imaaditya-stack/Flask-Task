75d47555c6417887c0907c76fb12fc69ac981fc9
imaaditya-stack <ghagaaditya21@gmail.com>
2021-05-02 23:29:21 +0530

    final commit

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-from flask import Flask, render_template, request, flash, logging, redirect, session, url_for
+from flask import Flask, render_template, request, flash, logging, redirect, session, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import desc
-from werkzeug.security import generate_password_hash, check_password_hash 
-from functools import wraps
 from datetime import datetime
 from pyzbar import pyzbar
 from werkzeug.utils import secure_filename
@@ -14,8 +11,11 @@ import pandas as pd
 app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ey.db'
 app.secret_key = "151FAHUI216892GAOP"
+
 db = SQLAlchemy(app)
 
 class Data(db.Model):
@@ -27,41 +27,76 @@ class Data(db.Model):
     
 db.create_all()
 
-@app.route("/")
+# ROUTES
+
+@app.route("/", methods = ['GET', 'POST'])
 def home(): 
-    return render_template('index.html')
+    total_docs = Data.query.count()
+    with_qr_code = Data.query.filter_by(has_qr_code = True).count()
+    without_qr_code = total_docs - with_qr_code
+    context = {'total_docs': total_docs, 'with_qr_code': with_qr_code, 'without_qr_code': without_qr_code}
+    return render_template('index.html', context=context)
 
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
-      files = request.files.getlist('file')
+
+      files = request.files.getlist('files[]')
+
+    #   print(files)
+
       for f in files:
-        data = Data()
-        data.file_name = f.filename
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(f.filename))
-        f.save(file_path)
+        has_qr_code = False
+        file_path = save_file(f)
         doc = fitz.open(file_path)
         image_list = doc.getPageImageList(0)
         for image in image_list:
-            xref = image[0]
-            pix = fitz.Pixmap(doc, xref)
-            if pix.n < 5:
-                pix.writeImage(f'{xref}.png')
-                image = cv2.imread(f'{xref}.png')
-                barcodes = pyzbar.decode(image)
-                if len(barcodes) > 0:
-                    for barcode in barcodes:
-                        df = pd.DataFrame({'qrcode': [barcode.data.decode('utf-8')], 'file_name': [f.filename]})
-                        df.to_csv('result.csv')
-                        data.has_qr_code = True
-                        db.session.add(data)
-                        db.session.commit()
-                else:
-                    print("No barcode")
-        data.has_qr_code = False
-        db.session.add(data)
-        db.session.commit()
-      return 'file uploaded successfully'
+            image = save_extracted_image(doc, image[0])
+            barcodes = pyzbar.decode(image)
+            if len(barcodes) > 0:
+                for barcode in barcodes:
+                    generate_excel_file(barcode.data.decode('utf-8'), f.filename)
+                    has_qr_code = True
+        
+        save_to_db(f.filename, has_qr_code)
+
+      return jsonify(status=200)
+
+
+# UTLIS FUNCTIONS
+
+def save_file(file):
+
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(file.filename))
+
+    file.save(file_path)
+
+    return file_path
+
+def generate_excel_file(data, filename):
+
+    df = pd.DataFrame({'qrcode': [data], 'file_name': [filename]})
+
+    df.to_excel('output.xlsx')
+
+def save_to_db(filename, has_qr_code):
+
+    data = Data(file_name=filename, has_qr_code=has_qr_code)
+
+    db.session.add(data)
+
+    db.session.commit()
+
+def save_extracted_image(doc, xref):
+
+    pix = fitz.Pixmap(doc, xref)
+
+    if pix.n < 5:
+        pix.writeImage(os.path.join(app.config['UPLOAD_FOLDER'], 'images', f'{xref}.png'))
+    
+    image = cv2.imread(os.path.join(app.config['UPLOAD_FOLDER'], 'images', f'{xref}.png'))    
+    
+    return image
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/js/index.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  var formData = new FormData();
+  $("#formFileMultiple").change(function () {
+    var totalfiles = document.getElementById("formFileMultiple").files.length;
+    for (var index = 0; index < totalfiles; index++) {
+      formData.append(
+        "files[]",
+        document.getElementById("formFileMultiple").files[index]
+      );
+    }
+  });
+
+  $("form").submit((e) => {
+    e.preventDefault();
+    $(".spinner-border").show();
+    $.ajax({
+      url: "/uploader",
+      type: "POST",
+      data: formData,
+      processData: false,
+      contentType: false,
+      success: function (data) {
+        // console.log(data);
+        $(".spinner-border").hide();
+        $(".alert").show();
+      },
+      error: function (error) {
+        alert("Something Went Wrong !");
+      },
+    });
+  });
+});
